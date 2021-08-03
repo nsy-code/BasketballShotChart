01ab2905a2c788f5f73bdee11d5badf7375c71c9
nsy <siuyung3@gmail.com>
2021-08-03 19:27:37 +0800

    bugfixed: dashboard display

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -6,8 +6,6 @@ import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import ShotChart from "../charts/ShotChart";
 import Dialog from "../Dialog";
-import "./Dashboard.css";
-
 
 const Dashboard = () => {
     const [value, setValue] = useLocalStorage("shot-data");
@@ -20,44 +18,53 @@ const Dashboard = () => {
     };
 
     const onSliderChangeHandler = (e) => {
-        setShotThreshold(parseInt(e.target.value) );
+        setShotThreshold(parseInt(e.target.value));
     };
 
     return (
-        <div className="dashboard-container mt-2">
-            <Card style={{ width: "550px" }}>
-                <CardContent>
-                    <div className="data-view">
-                        <ShotChart
-                            data={value}
-                            minCount={shotThreshold}
-                            isDisplayTips={true}
-                        />
-                        <div className="d-flex">
-                            <span>Shot Threshold: {shotThreshold} </span>
-                            <input className="ms-2" type="range" onChange={onSliderChangeHandler} value={shotThreshold} min="1" max="10" />
+        <div className="container">
+            <div className="row d-flex justify-content-center mt-2">
+                <Card style={{ width: "550px" }}>
+                    <CardContent>
+                        <div className="data-view">
+                            <ShotChart
+                                data={value}
+                                minCount={shotThreshold}
+                                isDisplayTips={true}
+                            />
+                            <div className="d-flex">
+                                <span>Shot Threshold: {shotThreshold} </span>
+                                <input
+                                    className="ms-2"
+                                    type="range"
+                                    onChange={onSliderChangeHandler}
+                                    value={shotThreshold}
+                                    min="1"
+                                    max="10"
+                                />
+                            </div>
                         </div>
-                    </div>
-                </CardContent>
-                <CardActions className="d-flex justify-content-end">
-                    <Button color="secondary" onClick={() => setOpen(true)}>
-                        Clear All Data
-                    </Button>
-                    <a
-                        href={`data:text/json;charset=utf-8,${encodeURIComponent(
-                            JSON.stringify(value)
-                        )}`}
-                        download="shotData.json"
-                    >
-                        {`Download Json`}
-                    </a>
-                </CardActions>
-            </Card>
-            <Dialog
-                isOpen={open}
-                handleClose={() => setOpen(false)}
-                handleConfirm={onConfirmHandler}
-            />
+                    </CardContent>
+                    <CardActions className="d-flex justify-content-end">
+                        <Button color="secondary" onClick={() => setOpen(true)}>
+                            Clear All Data
+                        </Button>
+                        <a
+                            href={`data:text/json;charset=utf-8,${encodeURIComponent(
+                                JSON.stringify(value)
+                            )}`}
+                            download="shotData.json"
+                        >
+                            {`Download Json`}
+                        </a>
+                    </CardActions>
+                </Card>
+                <Dialog
+                    isOpen={open}
+                    handleClose={() => setOpen(false)}
+                    handleConfirm={onConfirmHandler}
+                />
+            </div>
         </div>
     );
 };
