448d810ba459e08fd8ad45b1c0794e6f1ffd8f95
nsy <siuyung3@gmail.com>
2021-08-03 19:48:07 +0800

    updated: move nav to bottom

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,13 +3,14 @@ import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import DashboardIcon from "@material-ui/icons/Dashboard";
 import GestureIcon from "@material-ui/icons/Gesture";
-import DescriptionIcon from '@material-ui/icons/Description';
+import DescriptionIcon from "@material-ui/icons/Description";
 
 const NavBar = (props) => {
     const { history } = props;
     const [value, setValue] = React.useState("");
     return (
         <BottomNavigation
+            style={{ width: "100%", position: "fixed", bottom: 0 }}
             value={value}
             onChange={(event, newValue) => {
                 setValue(newValue);
